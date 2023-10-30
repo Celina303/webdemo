378a1ab0340520e4992d410e0e8a92eb28e66274
Celina303 <vanchinhnguyen2k3@gmail.com>
2023-10-31 00:08:02 +0700

    demo

---
+++ contact.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/social share buttons.css">
+    <title>Liên Hệ</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- header -->
+        <header
+            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <a href="#" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none logo"><img
+                    src="img/logo.png" width="150" height="60" />
+            </a>
+
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="" class="nav-link px-2 ">Trang chủ</a></li>
+                <li><a href="#" class="nav-link px-2 ">Giới thiệu</a></li>
+                <li><a href="#" class="nav-link px-2 ">Thú cưng</a></li>
+                <li><a href="contact.php" class="nav-link px-2 ">Liên hệ</a></li>
+            </ul>
+
+            <div class="col-md-3 text-end">
+                <a href="cart.php" class="item me-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                        <path
+                            d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg></a>
+                <a href="#" class="item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                    </svg></a>
+            </div>
+        </header>
+        <!-- end header -->
+        <main>
+            <!-- contact -->
+            <div class="background"></div>
+            <div class="contact-container">
+                <div class="contact-box">
+                    <div class="contact-left">
+                        <h1>Liên hệ với chúng tôi</h1>
+                        <table>
+                            <tr>
+                                <td>Email</td>
+                                <td>Gaugaustore123@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Số điện thoại</td>
+                                <td>0674812274</td>
+                            </tr>
+                            <tr>
+                                <td>Địa chỉ</td>
+                                <td>33 Tạ Quang Bửu, phường Bách Khoa <br>
+                                    quận Hai Bà Trưng, Hà</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="contact-right">
+                        <form>
+                            <div class="input-row">
+                                <div class="input-group">
+                                    <label> Họ và tên</label>
+                                    <input type="text" placeholder="">
+                                </div>
+                                <div class="input-group">
+                                    <label> Số điện thoại</label>
+                                    <input type="text" placeholder="">
+                                </div>
+                                <div class="Email">
+                                    <label> Email</label>
+                                    <input type="text" placeholder="">
+                                </div>
+                            </div>
+                            <label>Lời nhắn</label>
+                            <textarea rows="7"></textarea>
+                            <button type="submit">Gửi</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- end contact -->
+            <!-- social button -->
+            <div class="social-share">
+                <a href="https://www.facebook.com" class="facebook-share" target="_blank">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a href="https://www.messenger.com" class="messenger-share" target="_blank">
+                    <i class="bi bi-messenger"></i>
+                </a>
+                <a href="sms:1234567890" class="sms-share">
+                    <i class="bi bi-chat"></i>
+                </a>
+                <a href="tel:1234567890" class="phone-share">
+                    <i class="bi bi-telephone"></i>
+                </a>
+            </div>
+            <!-- end social button -->
+        </main>
+        <!-- footer -->
+        <footer>
+            <div class="footer-container">
+                <div class="footer-content right">
+                    <h3>CHÚNG TÔI CÓ</h3>
+                    <ul class="list">
+                        <li>
+                            <a href="#">Trang chủ</a>
+                        </li>
+                        <li>
+                            <a href="#">Giới thiệu</a>
+                        </li>
+                        <li>
+                            <a href="#">Thú cưng</a>
+                        </li>
+                        <li><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="footer-content middle">
+                    <img src="img/logo2.png" width="330px" height="100px">
+                    <p>
+                        Shop chó cảnh có sẵn nhiều bé chó xinh đẹp để lựa chọn và đón được ngay luôn. Bán Chó Cảnh Thuần
+                        Chủng Uy Tín .Giảm Giá khi mua bé thứ 2.
+                    </p>
+                </div>
+                <div class="footer-content left">
+                    <h3>THÔNG TIN LIÊN HỆ</h3>
+                    <div right-content>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z" />
+                                <path
+                                    d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034v.21Zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791Z" />
+                            </svg>
+                            Gaugaustore123@gmail.com</p>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                            </svg>
+                            0674812274</ơ>
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                            33 Tạ Quang Bửu, phường Bách Khoa quận Hai Bà Trưng, Hà</p>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+        <!-- end -->
+    </div>
+</body>
